18d203e1a6a79c52132e559014c58b9651099f5a
Philippe Martin <philippe@cryptobay.space>
2023-10-02 08:53:55 +0200

    add notes in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,6 +13,9 @@ app.use(json({ limit: "50mb" }));
 app.use(urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+// Setting the Access-Control-Allow-Origin header to "*" allows any domain to interact with your API, 
+// which might pose a security risk. If you know the specific domains that will need access, it would be
+//  better to specify those instead of allowing all.
 app.get("/up", (req: Request, res: Response) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.send("All Systems Up");
